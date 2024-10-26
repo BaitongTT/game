7ceda3d5a01b1dca180b35d98ce1425ca9f4c5eb
NatkritaS <b.natkriita@gmail.com>
2024-10-26 21:33:19 +0700

    Update Button.py

--- Button.py
+++ Button.py
@@ -1,10 +1,20 @@
 import pygame
 
 class Button:
-    def __init__(self, image_path, position):
+    def __init__(self, image_path, position, scale = 1.0):
         self.image = pygame.image.load(image_path).convert_alpha()
         self.rect = self.image.get_rect(topleft = position)
     
     def draw(self, window):
         window.blit(self.image, self.rect)
+        
+    def is_pressed(self):
+        mouse_pos = pygame.mouse.get_pos()
+        mouse_pressed = pygame.mouse.get_pressed()[0]
+        
+        if self.rect.collidepoint(mouse_pos):
+            if mouse_pressed:
+                return True
+            
+        return False
         
