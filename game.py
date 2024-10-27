dfda5a25f3d93fcff275856e6f161f9ba272bbd1
BaitongTT <thawanrat.pha@dome.tu.ac.th>
2024-10-27 22:24:33 +0700

    moving ground

--- game.py
+++ game.py
@@ -3,22 +3,13 @@ pygame.init()
 
 #framerate
 clock = pygame.time.Clock()
-FPS = 120
+FPS = 80
 
 #set display
 pygame.display.set_caption('Trick or Treat' )
 screen = pygame.display.set_mode((720, 400))
 width, length = screen.get_size() #get the windows size
 #print(width, length) to get 1536, 864
-
-#background
-background = pygame.image.load("Image/background_2.png").convert_alpha()
-scale =1.2
-dirt_block = pygame.image.load("Image/block.png").convert_alpha()
-dirt_block = pygame.transform.scale(dirt_block,(dirt_block.get_width() / scale, 
-    dirt_block.get_height() / scale))
-dirt_block_rect = dirt_block.get_rect()
-dirt_block_rect.bottomleft = ((0, 400))
 
 #keyboard control
 movetothe_left = False
@@ -60,13 +51,41 @@ class character1(pygame.sprite.Sprite):
 
 player = character1(55, 288, 5, 2)
 
+def load_and_scale_image(path, scale):
+    image = pygame.image.load(path)
+    return pygame.transform.scale(image, (image.get_width() // scale, image.get_height() // scale))
+
+background = load_and_scale_image("Image/background_2.png", 1)
+
+class DirtBlock(pygame.sprite.Sprite):
+    def __init__(self, x, y, scale):
+        super().__init__()
+        self.image = load_and_scale_image("Image/block.png", 1)
+        self.rect = self.image.get_rect(topleft=(x, y))
+
+# Create a group for dirt blocks
+dirt_blocks = pygame.sprite.Group()
+
+# Add multiple dirt blocks to the group
+for i in range(0, 3000, 40):  # start, how long, space
+    block = DirtBlock(i, 361, 9)
+    dirt_blocks.add(block)
+
+#moving objects
+speed = 1
+def move_objects_for_right(speed, move):
+    if move:
+        for block in dirt_blocks:
+            block.rect.x -= speed
+
 run = True
 while run:
     clock.tick(FPS)
     screen.blit(background, (0,0))
-    screen.blit(dirt_block, dirt_block_rect)
     player.draw()
     player.move(movetothe_left, movetothe_right)
+    dirt_blocks.draw(screen)
+    move_objects_for_right(speed, movetothe_right)
 
     for event in pygame.event.get():  
         if event.type == pygame.QUIT:  
