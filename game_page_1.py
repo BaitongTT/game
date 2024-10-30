2650b8e11609f254234ebb5df3cf15cdd75cf2ed
ppchkr <ppchkr0218@gmail.com>
2024-10-30 23:46:31 +0700

    Update game_page_1.py

--- game_page_1.py
+++ game_page_1.py
@@ -12,7 +12,7 @@ width, length = screen.get_size() #get the windows size
 #print(width, length) to get 1536, 864
 screen.fill((0, 0, 0))
 
-# button function
+#button function
 class Button:
     def __init__(self, image_path, position, scale = 1.0):
         self.image = pygame.image.load(image_path).convert_alpha()
@@ -33,27 +33,23 @@ class Button:
 
 #load images
 background_start = pygame.image.load("Image/background_1.png").convert_alpha()
-select_charecters = pygame.image.load("Image/select_charecter.png").convert_alpha()
-background_charecter_1 = pygame.image.load("Image/select_1.png").convert_alpha()
-background_charecter_2 = pygame.image.load("Image/select_2.png").convert_alpha()
-background_charecter_3 = pygame.image.load("Image/select_3.png").convert_alpha()
+select_characters = pygame.image.load("Image/select_charecter.png").convert_alpha()
+background_character_1 = pygame.image.load("Image/select_1.png").convert_alpha()
+background_character_2 = pygame.image.load("Image/select_2.png").convert_alpha()
+background_character_3 = pygame.image.load("Image/select_3.png").convert_alpha()
 
 # set button
-button_start = Button("Image/button_start.png" ,(300,300))  #Button เป็นการกำหนดว่ารูปนี้คือปุ่ม
+button_start = Button("Image/button_start.png" ,(300,320))  #Button เป็นการกำหนดว่ารูปนี้คือปุ่ม
 button_howtoplay= Button("Image/button_howtoplay.png",(0,250))
-button_play = Button("Image/button_play.png",(0,300))
-button_back = Button("Image/button_back.png",(0,300))
-charecters_1 = Button("Image/1_charecter.png",(60,133))
-charecters_2 = Button("Image/2_charecter.png",(269,135))
-charecters_3 = Button("Image/3_charecter.png",(479,135))
+character_1 = Button("Image/1_charecter.png",(60,133))
+character_2 = Button("Image/2_charecter.png",(269,135))
+character_3 = Button("Image/3_charecter.png",(479,135))
+
 
 # variable
-start_value = False
-select_charecters_value = False
-charecters_1_value = False
-charecters_2_value = False
-charecters_3_value = False
-
+button_value = False
+select_characters_value = False
+character_value = False
 
 #game loop
 run = True
@@ -64,14 +60,20 @@ while run:
     button_start.draw(screen)
     #button start
     if button_start.is_pressed():
-        start_value = True
-    if start_value == True:
-        screen.blit(select_charecters,(0,0))
-        select_charecters_value = True
-    if select_charecters_value == True:
-        charecters_1.draw(screen)
-        charecters_2.draw(screen)
-        charecters_3.draw(screen)
+        button_value = True
+    if button_value == True:
+        screen.blit(select_characters,(0,0))
+        character_1.draw(screen)
+        character_2.draw(screen)
+        character_3.draw(screen)
+        if character_1.is_pressed():
+            screen.blit(background_character_1,(0,0))
+        if character_2.is_pressed():
+            screen.blit(background_character_2,(0,0))
+        if character_3.is_pressed():
+            screen.blit(background_character_3,(0,0))
+
+        
     
         
         
