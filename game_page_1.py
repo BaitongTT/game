65203bdd679902eb6925e47d7f27c35a493b4528
ppchkr <ppchkr0218@gmail.com>
2024-10-31 00:36:01 +0700

    Update game_page_1.py

--- game_page_1.py
+++ game_page_1.py
@@ -1,5 +1,5 @@
 import pygame
-import Button
+from Button import Button
 pygame.init()
 
 #framerate
@@ -11,25 +11,6 @@ screen = pygame.display.set_mode((720, 400))
 width, length = screen.get_size() #get the windows size
 #print(width, length) to get 1536, 864
 screen.fill((0, 0, 0))
-
-#button function
-class Button:
-    def __init__(self, image_path, position, scale = 1.0):
-        self.image = pygame.image.load(image_path).convert_alpha()
-        self.rect = self.image.get_rect(topleft = position)
-    
-    def draw(self, window):
-        window.blit(self.image, self.rect)
-        
-    def is_pressed(self):
-        mouse_pos = pygame.mouse.get_pos()
-        mouse_pressed = pygame.mouse.get_pressed()[0]
-        
-        if self.rect.collidepoint(mouse_pos):
-            if mouse_pressed:
-                return True
-            
-        return False
 
 #load images
 background_start = pygame.image.load("Image/background_1.png").convert_alpha()
@@ -44,12 +25,18 @@ button_howtoplay= Button("Image/button_howtoplay.png",(0,250))
 character_1 = Button("Image/1_charecter.png",(60,133))
 character_2 = Button("Image/2_charecter.png",(269,135))
 character_3 = Button("Image/3_charecter.png",(479,135))
+button_back = Button("Image/button_back.png",(252,327))
+button__play = Button("Image/button_play.png",(365,327))
 
 
 # variable
 button_value = False
 select_characters_value = False
-character_value = False
+character_value_1 = False
+character_value_2 = False
+character_value_3 = False
+back_value = False
+play_value = False
 
 #game loop
 run = True
@@ -59,19 +46,59 @@ while run:
     screen.blit(background_start,(0,0))
     button_start.draw(screen)
     #button start
-    if button_start.is_pressed():
-        button_value = True
-    if button_value == True:
-        screen.blit(select_characters,(0,0))
-        character_1.draw(screen)
-        character_2.draw(screen)
-        character_3.draw(screen)
-        if character_1.is_pressed():
+    while True:
+        if button_start.is_pressed():
+            button_value = True
+        if button_value == True:
+            screen.blit(select_characters,(0,0))
+            character_1.draw(screen)
+            character_2.draw(screen)
+            character_3.draw(screen)
+            
+            if character_1.is_pressed():
+                character_value_1 = True
+                break
+            elif character_2.is_pressed():
+                character_value_2 = True
+                break
+            elif character_3.is_pressed():
+                character_value_3 = True
+                break
+                
+        if character_value_1 == True:
             screen.blit(background_character_1,(0,0))
-        if character_2.is_pressed():
+            button__play.draw(screen)
+            button_back.draw(screen)
+            if button_back.is_pressed():
+                back_value = True
+            elif button__play.is_pressed():
+                play_value = True
+                break
+            
+        if character_value_2 == True:
             screen.blit(background_character_2,(0,0))
-        if character_3.is_pressed():
+            button__play.draw(screen)
+            button_back.draw(screen)
+            if button_back.is_pressed():
+                back_value = True
+                break
+            elif button__play.is_pressed():
+                play_value = True
+                break
+        if character_value_3 == True:
             screen.blit(background_character_3,(0,0))
+            button__play.draw(screen)
+            button_back.draw(screen)
+            if button_back.is_pressed():
+                back_value = True
+            elif button__play.is_pressed():
+                play_value = True
+            
+        if back_value == True:
+            button_value = True
+        if play_value == True:
+            print(111)
+            break
 
         
     
